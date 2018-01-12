c41d4e2cf2dedbb08ade0031410ccc68c73a73f6
jiazhen-mac-01 <18896591232@163.com>
2018-01-12 16:36:45 +0800

    hebing

--- TrendBetting_0531换首页/TrendBetting/ViewControllers/设置/下载文件/TBDayDataViewController.m
+++ TrendBetting_0531换首页/TrendBetting/ViewControllers/设置/下载文件/TBDayDataViewController.m
@@ -59,45 +59,34 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(errNotificationAction:) name:@"DownErrNotification" object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(sucNotificationAction:) name:@"InfoNotification" object:nil];
     
-    NSString*monthFileNameStr=[NSString stringWithFormat:@"%@号/2017-%@",self.selectedRoom,self.selectedMonth];
+    NSString*monthFileNameStr=[NSString stringWithFormat:@"%@号/%@",self.selectedRoom,self.selectedMonth];
     daysArr=[[Utils sharedInstance] getAllFileName:monthFileNameStr];/////月份里的数据
-    
-    NSDate *now = [NSDate date];
-    NSLog(@"now date is: %@", now);
-    NSCalendar *calendar = [NSCalendar currentCalendar];
-    NSUInteger unitFlags = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit;
-    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:now];
-    
-    int year =(int) [dateComponent year];
-    int month = (int) [dateComponent month];
-    int day = (int) [dateComponent day];
 
-    
     dayDataArr = [[NSMutableArray alloc]init];
-    NSArray *arr1 = @[@"01",@"03",@"05",@"07",@"08",@"10",@"12"];
-    
 
-    if ([self.selectedMonth integerValue] > month) {
-        allday = 1;
-    }else if ([self.selectedMonth integerValue] == month) {
-        allday = [self.currentAllDay integerValue];
-    }else if ([self.selectedMonth integerValue] == 2) {
-        if (year%4 == 0) {
-            allday = 29;
-        }else{
-            allday = 28;
+    NSDateFormatter*formatter =[[NSDateFormatter alloc]init];
+    [formatter setDateFormat:@"yyyy-MM"];
+    if ([_selectedMonth isEqualToString:[formatter stringFromDate:[NSDate date]]])
+    {
+        NSCalendar *calendar = [NSCalendar currentCalendar];
+        NSUInteger unitFlags = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit;
+        NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:[NSDate date]];
+        int hour = (int)[dateComponent hour];
+        int minute = (int)[dateComponent minute];
+        if (hour>11||(hour==11&&minute>=15)) {
+            dateComponent = [calendar components:unitFlags fromDate:[NSDate dateWithTimeInterval:24*60*60 sinceDate:[NSDate date]]];
         }
-    }else if([arr1 containsObject:self.selectedMonth]){
-        allday = 31;
-    }else{
-        allday = 30;
+         allday = (int)[dateComponent day];
+    } else {
+        allday =[[Utils sharedInstance]getAllDayFromDate:[formatter dateFromString:_selectedMonth]];
+
     }
-    
     for (NSInteger i =allday; i>0; i--) {
         NSString *dayDataStr = [NSString stringWithFormat:@"%ld.txt",i];
         [dayDataArr addObject:dayDataStr];
     }
-    
+    _mytableView.tableFooterView=[[UIView alloc]init];
+
     
     
     // Do any additional setup after loading the view.
@@ -137,9 +126,9 @@
 #pragma mark -- UITableViewDelegate
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-//    [[Utils sharedInstance] initSetTenModel];
-//    NSDictionary *dic = [[NSDictionary alloc]initWithObjectsAndKeys:self.selectedRoom,@"selectedRoom",monthStrArr[indexPath.row],@"selectedRoom", nil];
-//    [self performSegueWithIdentifier:@"showDayVC" sender:dic];
+    //    [[Utils sharedInstance] initSetTenModel];
+    //    NSDictionary *dic = [[NSDictionary alloc]initWithObjectsAndKeys:self.selectedRoom,@"selectedRoom",monthStrArr[indexPath.row],@"selectedRoom", nil];
+    //    [self performSegueWithIdentifier:@"showDayVC" sender:dic];
     
 }
 
@@ -148,14 +137,14 @@
 {
     // 下载按钮点击
     if ([Utils sharedInstance].isNetwork) {
-        NSLog(@"下载文件:%@号 2017-%@ %ld.txt",self.selectedRoom,self.selectedMonth,tag);
-        NSString *timeStr = [NSString stringWithFormat:@"2017-%@-%ld",self.selectedMonth,tag];
+        NSLog(@"下载文件:%@号 %@ %ld.txt",self.selectedRoom,self.selectedMonth,tag);
+        NSString *timeStr = [NSString stringWithFormat:@"%@-%ld",self.selectedMonth,tag];
         selectIndex =allday - tag;
         [[Utils sharedInstance] downLoadServerFile:self.selectedRoom timeStr:timeStr isanimated:YES];
     } else {
-         [self.view makeToast:@"请检查网络设置" duration:3.0f position:CSToastPositionCenter];
+        [self.view makeToast:@"请检查网络设置" duration:3.0f position:CSToastPositionCenter];
     }
-   
+
     
 }
 
@@ -163,36 +152,36 @@
 #pragma mark -- notification
 -(void)nofileNotificationAction:(NSNotification *)d
 {
-//    NSDictionary *userInfo = d.userInfo;
+    //    NSDictionary *userInfo = d.userInfo;
     NSString *tipStr = [NSString stringWithFormat:@"文件不存在"];
     [self.view makeToast:tipStr duration:3.0f position:CSToastPositionCenter];
-//    [SVProgressHUD showErrorWithStatus:tipStr];
+    //    [SVProgressHUD showErrorWithStatus:tipStr];
     
 }
 
 -(void)errNotificationAction:(NSNotification *)d
 {
-//    NSDictionary *userInfo = d.userInfo;
-     NSString *tipStr = [NSString stringWithFormat:@"下载超时"];
-     [self.view makeToast:tipStr duration:3.0f position:CSToastPositionCenter];
-//    [SVProgressHUD showErrorWithStatus:tipStr];
+    //    NSDictionary *userInfo = d.userInfo;
+    NSString *tipStr = [NSString stringWithFormat:@"下载超时"];
+    [self.view makeToast:tipStr duration:3.0f position:CSToastPositionCenter];
+    //    [SVProgressHUD showErrorWithStatus:tipStr];
     
 }
 
 -(void)sucNotificationAction:(NSNotification *)d
 {
-//    NSDictionary *userInfo = d.userInfo;
+    //    NSDictionary *userInfo = d.userInfo;
     
     NSString *tipStr = [NSString stringWithFormat:@"下载成功"];
-    NSString*monthFileNameStr=[NSString stringWithFormat:@"%@号/2017-%@",self.selectedRoom,self.selectedMonth];
+    NSString*monthFileNameStr=[NSString stringWithFormat:@"%@号/%@",self.selectedRoom,self.selectedMonth];
     daysArr=[[Utils sharedInstance] getAllFileName:monthFileNameStr];/////月份里的数据
-     [self.view makeToast:tipStr duration:3.0f position:CSToastPositionCenter];
-//    [SVProgressHUD showErrorWithStatus:tipStr];
+    [self.view makeToast:tipStr duration:3.0f position:CSToastPositionCenter];
+    //    [SVProgressHUD showErrorWithStatus:tipStr];
     NSIndexPath *index = [NSIndexPath indexPathForRow:selectIndex inSection:0];
     dayCell *cell = [_mytableView cellForRowAtIndexPath:index];
     cell.yicunzai.hidden = NO;
     [_mytableView reloadRowsAtIndexPaths:@[index] withRowAnimation:UITableViewRowAnimationNone];
-//    [_mytableView reloadData];
+    //    [_mytableView reloadData];
 }
 
 
@@ -203,13 +192,13 @@
 }
 
 /*
-#pragma mark - Navigation
+ #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
--- TrendBetting_0531换首页/TrendBetting/ViewControllers/设置/下载文件/TBMonthViewController.m
+++ TrendBetting_0531换首页/TrendBetting/ViewControllers/设置/下载文件/TBMonthViewController.m
@@ -24,7 +24,6 @@
 @interface TBMonthViewController ()<UITableViewDelegate,UITableViewDataSource>
 {
     NSMutableArray*allMonthArr;
-    NSMutableArray*monthStrArr;
     NSString *allday;
 }
 
@@ -36,85 +35,20 @@
     [super viewDidLoad];
     self.title=[NSString stringWithFormat:@"%@号房间",self.selectedRoom];
     self.edgesForExtendedLayout=UIRectEdgeNone;
-    
-    NSDate *now = [NSDate date];
-    NSLog(@"now date is: %@", now);
-    NSCalendar *calendar = [NSCalendar currentCalendar];
-    NSUInteger unitFlags = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit;
-    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:now];
-    
-    int year =(int) [dateComponent year];
-    int month = (int) [dateComponent month];
-    int day = (int) [dateComponent day];
-    
-    NSDate *nowDate = [NSDate date];
-    
-    NSString *str = [NSString stringWithFormat:@"%d-%02d-%02d 11:15:00 +0800",year,month,day];
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss Z";
-
-    NSDate *lastDate = [formatter dateFromString:str];
-    
-    NSArray *monthArr = @[@"1",@"3",@"5",@"7",@"8",@"10",@"12"];
-    NSString *monthStr = [NSString stringWithFormat:@"%d",month];
-    if ([nowDate timeIntervalSince1970]>[lastDate timeIntervalSince1970]) {
-        if ([monthArr containsObject:monthStr]) {
-            if (day == 31) {
-                if (month == 12) {
-                    year = year + 1;
-                    month = 1;
-                    day = 1;
-                }else{
-                    day = 1;
-                    month = month + 1;
-                }
-            }else{
-                day = day + 1;
-            }
-        }else if(month == 2)
-        {
-            if (year % 4 == 0) {
-                if (day == 29) {
-                    day = 1;
-                    month = month + 1;
-                }else{
-                    day = day + 1;
-                }
-            }else{
-                if (day == 28) {
-                    day = 1;
-                    month = month + 1;
-                }else{
-                    day = day + 1;
-                }
-            }
-        }else
-        {
-            if (day == 30) {
-                day = 1;
-                month = month + 1;
-            }else{
-                day = day + 1;
-            }
-        }
-        allday = [NSString stringWithFormat:@"%d",day];
-    }
-
-    
-    
-    self.navigationController.navigationBarHidden=NO;
-    
     allMonthArr = [[NSMutableArray alloc]init];
-    monthStrArr = [[NSMutableArray alloc]init];
-    for (int i =month; i>=4; i--) {
-        NSString *mothStr = [NSString stringWithFormat:@"%d-%02d",year,i];
-        NSString *moth = [NSString stringWithFormat:@"%02d",i];
-        [allMonthArr addObject:mothStr];
-        [monthStrArr addObject:moth];
+    [allMonthArr addObjectsFromArray:[self getAllMonthFrom:@"2017-04" endDate:[NSDate date]]];
+
+}
+-(NSArray*)getAllMonthFrom:(NSString*)fromDate endDate:(NSDate*)endDate{
+    NSMutableArray*resultArr=[[NSMutableArray alloc]init];
+    NSDateFormatter*formatter =[[NSDateFormatter alloc]init];
+    [formatter setDateFormat:@"yyyy-MM"];
+    NSDate*beginDate =[formatter dateFromString:fromDate];
+    while ([[Utils sharedInstance] compareDate:beginDate endDate:endDate]) {
+        [resultArr addObject:[formatter stringFromDate:beginDate]];
+        beginDate = [[Utils sharedInstance] monthAddOneMonth:beginDate];
     }
-//    allMonthArr =[[NSMutableArray alloc] initWithArray:[[Utils sharedInstance] orderArr:allMonthArr isArc:NO]];
-    
-    // Do any additional setup after loading the view.
+    return resultArr;
 }
 
 
@@ -131,7 +65,7 @@
     monthCell *cell = [tableView dequeueReusableCellWithIdentifier:@"monthCell"];
     [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
     
-    cell.monthLab.text = allMonthArr[indexPath.row];
+    cell.monthLab.text = allMonthArr[allMonthArr.count-1-indexPath.row];
     
     return cell;
 }
@@ -143,7 +77,9 @@
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [[Utils sharedInstance] initSetTenModel];
-    NSDictionary *dic = [[NSDictionary alloc]initWithObjectsAndKeys:self.selectedRoom,@"selectedRoom",monthStrArr[indexPath.row],@"selectedMonth",allday,@"currentAllDay",nil];
+    NSDictionary *dic = @{@"selectedRoom":self.selectedRoom,
+                          @"selectedMonth":allMonthArr[allMonthArr.count-1-indexPath.row],
+                          @"currentAllDay":@""};
     [self performSegueWithIdentifier:@"showDayVC" sender:dic];
 
 }
