5320fda180f9402c051f5852335b4e0ba9b96790
yuxiuyu <18896591232@163.com>
2018-03-27 23:12:51 +0800

    隐藏下载啊功能的版本

--- TrendBetting_0531换首页/TrendBetting/ViewControllers/结果查看/TBResultViewController.m
+++ TrendBetting_0531换首页/TrendBetting/ViewControllers/结果查看/TBResultViewController.m
@@ -29,25 +29,25 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(sucNotificationAction:) name:@"InfoNotification" object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(nofileNotificationAction:) name:@"noFileInfoNotification" object:nil];
     //下载文件
-   
-     if ([Utils sharedInstance].isNetwork) {
-    mytimeCount=0;
-    [SVProgressHUD showWithStatus:@"文件下载中"];
-    NSArray*curDay=[[Utils sharedInstance] getCurrentYearMonthDay];
-    for (int i = 1; i<5; i++) {
-        NSString *timeStr = [NSString stringWithFormat:@"%d-%02d-%02d",[curDay[0] intValue],[curDay[1] intValue],[curDay[2] intValue]];
-        [[Utils sharedInstance] downLoadServerFile:[NSString stringWithFormat:@"%d",i] timeStr:timeStr isanimated:NO];
+    
+    if ([Utils sharedInstance].isNetwork) {
+//        mytimeCount=0;
+//        [SVProgressHUD showWithStatus:@"文件下载中"];
+//        NSArray*curDay=[[Utils sharedInstance] getCurrentYearMonthDay];
+//        for (int i = 1; i<5; i++) {
+//            NSString *timeStr = [NSString stringWithFormat:@"%d-%02d-%02d",[curDay[0] intValue],[curDay[1] intValue],[curDay[2] intValue]];
+//            [[Utils sharedInstance] downLoadServerFile:[NSString stringWithFormat:@"%d",i] timeStr:timeStr isanimated:NO];
+//        }
+    } else{
+        [self.view makeToast:@"请检查网络设置" duration:3.0f position:CSToastPositionCenter];
     }
-     } else{
-          [self.view makeToast:@"请检查网络设置" duration:3.0f position:CSToastPositionCenter];
-     }
     
     
     self.navigationController.navigationBarHidden=NO;
     _tableview.tableFooterView=[[UIView alloc]init];
     dataArray=@[@"读取新规则结果",@"读取今天的结果",@"K线图结果"];
-//    dataArray=@[@"读取的数据结果",@"我保存的数据结果",@"读取设置组结果",@"读取新规则结果"];
-//     dataArray=@[@"读取的数据结果",@"我保存的数据结果",@"读取设置组结果"];
+    //    dataArray=@[@"读取的数据结果",@"我保存的数据结果",@"读取设置组结果",@"读取新规则结果"];
+    //     dataArray=@[@"读取的数据结果",@"我保存的数据结果",@"读取设置组结果"];
     // Do any additional setup after loading the view.
 }
 
@@ -73,24 +73,24 @@
 }
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-//    if (indexPath.row==0)
-//    {
-//        [self performSegueWithIdentifier:@"showRoomDetailVC" sender:nil];
-//    }
-//    else if(indexPath.row==1)
-//    {
-//        [self getDataRead];
-//         [self performSegueWithIdentifier:@"showresultRoomVC" sender:@{@"selectedTitle":@"我保存的数据"}];
-//    }
-//    else if(indexPath.row==2)
-//    {
-//         [self performSegueWithIdentifier:@"show_groupRoomVC" sender:nil];
-//        
-//    }
-//    else{
+    //    if (indexPath.row==0)
+    //    {
+    //        [self performSegueWithIdentifier:@"showRoomDetailVC" sender:nil];
+    //    }
+    //    else if(indexPath.row==1)
+    //    {
+    //        [self getDataRead];
+    //         [self performSegueWithIdentifier:@"showresultRoomVC" sender:@{@"selectedTitle":@"我保存的数据"}];
+    //    }
+    //    else if(indexPath.row==2)
+    //    {
+    //         [self performSegueWithIdentifier:@"show_groupRoomVC" sender:nil];
+    //
+    //    }
+    //    else{
     [self performSegueWithIdentifier:@"show_newResultRoomVC" sender:@{@"isCurrentDay":[NSString stringWithFormat:@"%ld",indexPath.row]}];
     
-//    }
+    //    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -102,27 +102,27 @@
 {
     NSArray*monthsArr=[[Utils sharedInstance] getAllFileName:SAVE_DATA_FILENAME];
     NSMutableDictionary* housesDic=[[NSMutableDictionary alloc]init];
-
-        NSMutableDictionary*monthsDic=[[NSMutableDictionary alloc] init];
-        float winMoney=0;
-        for (NSString*monthstr in monthsArr)
+    
+    NSMutableDictionary*monthsDic=[[NSMutableDictionary alloc] init];
+    float winMoney=0;
+    for (NSString*monthstr in monthsArr)
+    {
+        NSString*monthFileNameStr=[NSString stringWithFormat:@"%@/%@",SAVE_DATA_FILENAME,monthstr];
+        NSArray*daysArr=[[Utils sharedInstance] getAllFileName:monthFileNameStr];/////月份里的数据
+        NSMutableDictionary*daysDic=[[NSMutableDictionary alloc]init];
+        for (NSString*dayStr in daysArr)
         {
-            NSString*monthFileNameStr=[NSString stringWithFormat:@"%@/%@",SAVE_DATA_FILENAME,monthstr];
-            NSArray*daysArr=[[Utils sharedInstance] getAllFileName:monthFileNameStr];/////月份里的数据
-            NSMutableDictionary*daysDic=[[NSMutableDictionary alloc]init];
-            for (NSString*dayStr in daysArr)
-            {
-                NSArray*array=[dayStr componentsSeparatedByString:@"."];
-                NSDictionary*tepDic=[[Utils sharedInstance] getDayData:monthFileNameStr dayStr:array[0]];
-                NSArray*tepArr=tepDic[@"daycount"];
-                winMoney=winMoney+[tepArr[5] floatValue];
-                [daysDic setObject:tepDic forKey:array[0]];
-            }
-            [monthsDic setObject:daysDic forKey:monthstr];
+            NSArray*array=[dayStr componentsSeparatedByString:@"."];
+            NSDictionary*tepDic=[[Utils sharedInstance] getDayData:monthFileNameStr dayStr:array[0]];
+            NSArray*tepArr=tepDic[@"daycount"];
+            winMoney=winMoney+[tepArr[5] floatValue];
+            [daysDic setObject:tepDic forKey:array[0]];
         }
+        [monthsDic setObject:daysDic forKey:monthstr];
+    }
     [housesDic setObject:monthsDic forKey:@"我保存的数据"];
     [Utils sharedInstance].housesDic=[[NSDictionary alloc] initWithDictionary:housesDic];
-  
+    
 }
 
 
@@ -132,11 +132,11 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
-//    if ([segue.identifier isEqualToString:@"showRoomDetailVC"])
-//    {
-        UIViewController*vc=[segue destinationViewController];
-        [vc setValuesForKeysWithDictionary:(NSDictionary*)sender];
-//    }
+    //    if ([segue.identifier isEqualToString:@"showRoomDetailVC"])
+    //    {
+    UIViewController*vc=[segue destinationViewController];
+    [vc setValuesForKeysWithDictionary:(NSDictionary*)sender];
+    //    }
 }
 
 
@@ -155,7 +155,7 @@
     [alert addAction:cancel];
     [alert addAction:reDown];
     [self presentViewController:alert animated:NO completion:nil];
-     mytimeCount++;
+    mytimeCount++;
     [self refreshData];
 }
 
@@ -171,15 +171,15 @@
 
 -(void)sucNotificationAction:(NSNotification *)d
 {
-//    NSDictionary *userInfo = d.userInfo;
+    //    NSDictionary *userInfo = d.userInfo;
     mytimeCount++;
     [self refreshData];
-
+    
 }
 
 -(void)refreshData{
     if (mytimeCount==4&&isappera) {
-         [self hidenProgress];
+        [self hidenProgress];
     }
 }
 -(void)viewWillDisappear:(BOOL)animated{
